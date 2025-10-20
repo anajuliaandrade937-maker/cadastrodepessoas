7e0b0ca1b9aee79bdea18db3392538f1c11db6b0
anajuliaandrade937-maker <anajuliandrade937gmail.com>
2025-10-20 10:01:43 -0300

    tarefa:cadastro de pessoa

---
+++ cadastro.py
@@ -0,0 +1,9 @@
+nome : str
+anonacimento:int
+cidade:str
+nome=input("informe seu nome")
+anonacimento=input("usoario infome seu nacimento")
+cidade=input("informe sua cidade")
+print(nome)
+print(anonacimento)
+print(cidade)
